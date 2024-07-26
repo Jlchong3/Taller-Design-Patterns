4c7ef35ab9aaeef9f215fd5dbc9d20113a43e524
DAVID SALOMON SUMBA CORREA <127973684+2Dsa3@users.noreply.github.com>
2024-07-25 23:34:13 -0500

    Add files via upload

---
+++ v1.4/Strategy/strategy/src/main/java/com/mycompany/strategy/Distribution.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.strategy;
+
+/**
+ *
+ * @author davsu
+ */
+public class Distribution implements DistributionStrategy{
+    
+    private DistributionStrategy strategy;
+    
+    
+    public void distribution(Store a, Store b){
+        
+    }
+    
+    public void setStrategy(DistributionStrategy str){
+        
+    }
+    
+    
+}
---
+++ v1.4/Strategy/strategy/src/main/java/com/mycompany/strategy/DistributionStrategy.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.strategy;
+
+/**
+ *
+ * @author davsu
+ */
+public interface DistributionStrategy {
+    
+    void distribution(Store A, Store B);
+}
